7b951690a7403d41a5ab5634d850a2753e3d54de
ParkHyunSu99 <phs0767@pineone.com>
2025-06-22 01:04:14 +0900

    로그인 화면 UI 그리기

---
+++ app/src/main/java/fastcampus/aos/part2/part2_chapter6/LoginActivity.kt
@@ -0,0 +1,68 @@
+package fastcampus.aos.part2.part2_chapter6
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import fastcampus.aos.part2.part2_chapter6.databinding.ActivityLoginBinding
+
+class LoginActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일 또는 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 회원가입 성공
+                        Toast.makeText(this, "회원가입에 성공했습니다.\n로그인해주세요.", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        // 회원가입 실패
+                        Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.signInButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일 또는 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 로그인 성공
+                        Toast.makeText(this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        // 로그인 실패
+                        Log.e("LoginActivity", "로그인 실패: ${task.exception}")
+                        Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+    }
+}
--- app/src/main/java/fastcampus/aos/part2/part2_chapter6/MainActivity.kt
+++ app/src/main/java/fastcampus/aos/part2/part2_chapter6/MainActivity.kt
@@ -1,20 +1,26 @@
 package fastcampus.aos.part2.part2_chapter6
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val currentUser = Firebase.auth.currentUser
+
+        if (currentUser == null) {
+            // 로그인이 되어 있지 않은 경우
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
     }
 }
